6ebe28755072f87e0a41f5e4cb81fce7075a7872
John <birukovie@gmail.com>
2023-09-18 13:43:23 +0300

    CRUD final

--- fullstack/product/urls.py
+++ fullstack/product/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     path('adm/categories/', views.CategoryListView.as_view(), name='category-list'),
     path('adm/categories/create/', views.CategoryCreateView.as_view(), name='category-create'),
     path('adm/categories/update/<int:pk>/', views.CategoryUpdateView.as_view(), name='category-update'),
-    path('adm/categories/delele/<int:id>/', views.CategoryDeleteView.as_view(), name='category-delete'),
+    path('adm/categories/delele/<int:pk>/', views.CategoryDeleteView.as_view(), name='category-delete'),
 
     path('adm/brands/', views.BrandListView.as_view(), name='brand-list'),
     path('adm/brands/create/', views.BrandCreateView.as_view(), name='brand-create'),
     path('adm/brands/update/<int:pk>/', views.BrandUpdateView.as_view(), name='brand-update'),
-    path('adm/brands/delete/<int:id>/', views.BrandDeleteView.as_view(), name='brand-delete'),
+    path('adm/brands/delete/<int:pk>/', views.BrandDeleteView.as_view(), name='brand-delete'),
 ]
