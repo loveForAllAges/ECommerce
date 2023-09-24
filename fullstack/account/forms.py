de37ae1af46d41fb9fbe9b4bbfe199f9eeaf6d7d
John <birukovie@gmail.com>
2023-09-24 19:47:44 +0300

    udpate acc forms

--- fullstack/account/forms.py
+++ fullstack/account/forms.py
@@ -9,12 +9,12 @@ from django.core.validators import validate_email
 
 
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(label='Введите имя', max_length=128)
-    last_name = forms.CharField(label='Введите фамилию', max_length=128)
-    email = forms.EmailField(max_length=255, error_messages={'invalid': 'Неверный формат!'})
+    first_name = forms.CharField(max_length=128)
+    last_name = forms.CharField(max_length=128)
+    email = forms.EmailField(error_messages={'invalid': 'Неверный формат!'})
     phone = forms.CharField(max_length=10)
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -22,8 +22,8 @@ class CustomUserCreationForm(UserCreationForm):
 
     def clean_phone(self):
         phone = self.cleaned_data['phone']
-        if len(phone) != 10 and phone.isdigit():
-            raise forms.ValidationError("Неверный номер телефона!")
+        if len(phone) != 10 or not phone.isdigit():
+            raise forms.ValidationError("Неверный формат номера телефона!")
         return phone
 
     def clean_password2(self):
@@ -44,18 +44,31 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs.update(
-            {'class': 'form-control mb-3', 'placeholder': 'Имя'})
-        self.fields['last_name'].widget.attrs.update(
-            {'class': 'form-control mb-3', 'placeholder': 'Фамилия'})
-        self.fields['phone'].widget.attrs.update(
-            {'class': 'form-control mb-3', 'placeholder': 'Номер телефона'})
-        self.fields['email'].widget.attrs.update(
-            {'class': 'form-control mb-3', 'placeholder': 'Почта', 'name': 'email', 'id': 'id_email'})
-        self.fields['password1'].widget.attrs.update(
-            {'class': 'form-control mb-3', 'placeholder': 'Пароль'})
-        self.fields['password2'].widget.attrs.update(
-            {'class': 'form-control', 'placeholder': 'Повторите пароль'})
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm', 
+            'placeholder': 'Введите имя',
+            "autofocus": True
+        })
+        self.fields['last_name'].widget.attrs.update({
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm', 
+            'placeholder': 'Фамилия'
+        })
+        self.fields['phone'].widget.attrs.update({
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm', 
+            'placeholder': 'Номер телефона'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm', 
+            'placeholder': 'Почта', 
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm', 
+            'placeholder': 'Пароль'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm', 
+            'placeholder': 'Повторите пароль'
+        })
 
 
 class CustomAuthenticationForm(AuthenticationForm):
@@ -63,28 +76,25 @@ class CustomAuthenticationForm(AuthenticationForm):
         max_length=255,
         error_messages={'invalid': 'Неверный формат!'},
         widget=forms.EmailInput(
-            attrs=
-            {
-                "autofocus": True,
-                'placeholder': 'Почта',
-            }
-            ),
+        attrs={
+            "autofocus": True,
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm', 
+            'placeholder': 'Введите почту',
+        }
+        ),
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
         attrs={
             "autocomplete": "current-password",
-            'class': 'form-control',
-            'placeholder': 'Пароль',
-            'id': 'login-pwd',
+            'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm',
+            'placeholder': 'Введите пароль',
         }
     ))
 
     error_messages = {
-        "invalid_login": (
-            "Неверная почта или пароль!"
-        ),
-        "inactive": ("Аккаунт не активирован!"),
+        "invalid_login": "Неверная почта или пароль!",
+        "inactive": "Аккаунт не активирован!",
     }
 
 
@@ -129,16 +139,15 @@ class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(
         label='Почта', max_length=255, widget=forms.EmailInput(
             attrs={
-                'class': '',
+                'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-50 disabled:text-gray-500',
                 'placeholder': 'Введите почту',
-                'readonly': 'readonly'
             }
         )
     )
     phone = forms.CharField(
         label='Номер телефона', max_length=10, widget=forms.TextInput(
             attrs={
-                'class': '',
+                'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm',
                 'placeholder': 'Введите номер телефона',
             }
         )
@@ -146,7 +155,7 @@ class UserUpdateForm(forms.ModelForm):
     first_name = forms.CharField(
         label='Имя', max_length=128, widget=forms.TextInput(
             attrs={
-                'class': '',
+                'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm',
                 'placeholder': 'Введите имя',
             }
         )
@@ -154,7 +163,7 @@ class UserUpdateForm(forms.ModelForm):
     last_name = forms.CharField(
         label='Фамилия', max_length=128, widget=forms.TextInput(
             attrs={
-                'class': '',
+                'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm',
                 'placeholder': 'Введите фамилию',
             }
         )
@@ -166,8 +175,8 @@ class UserUpdateForm(forms.ModelForm):
 
     def clean_phone(self):
         phone = self.cleaned_data['phone']
-        if len(phone) != 10 and phone.isdigit():
-            raise forms.ValidationError("Неверный номер телефона!")
+        if len(phone) != 10 or not phone.isdigit():
+            raise forms.ValidationError("Неверный формат номера телефона!")
         return phone
 
     def __init__(self, *args, **kwargs):
@@ -175,17 +184,16 @@ class UserUpdateForm(forms.ModelForm):
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
         self.fields['phone'].required = True
-        self.fields['email'].required = True
+        self.fields['email'].disabled = True
 
 
 class CustomPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(
         max_length=255,
-        label= 'Почта',
         error_messages={'invalid': 'Неверный формат!'},
         widget=forms.EmailInput(
             attrs={
-                'class': '', 
+                'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm',
                 'placeholder': 'Введите почту',
                 "autocomplete": "email"
             }
@@ -202,6 +210,7 @@ class CustomSetPasswordForm(SetPasswordForm):
         label="Новый пароль",
         widget=forms.PasswordInput(
             attrs={
+                'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm',
                 "autocomplete": "new-password",
                 'placeholder': 'Придумайте пароль'
             }
@@ -212,6 +221,7 @@ class CustomSetPasswordForm(SetPasswordForm):
         label="Повторите пароль",
         widget=forms.PasswordInput(
             attrs={
+                'class': 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm',
                 "autocomplete": "new-password",
                 'placeholder': 'Повторите новый пароль'
             }
