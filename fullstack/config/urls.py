5bd4c73378ba293796cdee634223b615edb35f31
John <birukovie@gmail.com>
2023-09-25 22:46:00 +0300

    update config

--- fullstack/config/urls.py
+++ fullstack/config/urls.py
@@ -1,26 +1,28 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
 
 
 def create_test_db(request):
-    from product.models import Brand, Product, Category
+    return render(request, 'errors/404.html')
+    # from product.models import Brand, Product, Category
 
-    for i in range(1, 10):
-        brand = Brand.objects.create(name=f'Brand {i}')
-        category = Category.objects.create(name=f'Category {i}')
+    # for i in range(1, 10):
+    #     brand = Brand.objects.create(name=f'Brand {i}')
+    #     category = Category.objects.create(name=f'Category {i}')
 
-        Product.objects.create(
-            name=f'Product {i}',
-            description=f'Description {i}',
-            price=i*10,
-            brand=brand,
-            category=category
-        )
+    #     Product.objects.create(
+    #         name=f'Product {i}',
+    #         description=f'Description {i}',
+    #         price=i*10,
+    #         brand=brand,
+    #         category=category
+    #     )
 
 
 urlpatterns = [
-    # path('create_test_db/', create_test_db),
+    # path('404/', create_test_db),
     path('address/', include('address.urls')),
     path('cart/', include('cart.urls')),
     path('wishlist/', include('wishlist.urls')),
@@ -28,6 +30,8 @@ urlpatterns = [
     path('', include('account.urls')),
     path('', include('product.urls')),
 ]
+
+# print(urlpatterns)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
