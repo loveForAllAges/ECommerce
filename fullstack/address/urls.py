a713ce327032fb8ceb43ac2511f764337e469170
John <birukovie@gmail.com>
2023-09-25 19:38:24 +0300

    update address urls

--- fullstack/address/urls.py
+++ fullstack/address/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('create/', views.AddressCreateView.as_view(), name='address-create'),
     path('make-main/', views.address_make_main, name='address-make-main'),
-    path('edit/<int:pk>/', views.AddressUpdateView.as_view(), name='address-update'),
     path('delete/', views.address_delete, name='address-delete'),
 ]
