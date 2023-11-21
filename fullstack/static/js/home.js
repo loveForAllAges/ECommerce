61c891d330c349efb940cf766ba2c0d2e8a24085
John <birukovie@gmail.com>
2023-11-22 00:28:08 +0300

    update

--- fullstack/static/js/home.js
+++ fullstack/static/js/home.js
@@ -1,7 +1,9 @@
 function getPageData() {
+    
     $.ajax({
         url: '/api/home',
         success: function(data) {
+            $('#pagePreloader').empty();
             data.forEach(element => {
                 var products = []
                 element.content.forEach(product => {
@@ -13,7 +15,7 @@ function getPageData() {
                     <div class="mx-auto max-w-7xl px-4 py-24 lg:py-32 sm:px-6 lb:px-8">
                         <div class="flex items-center justify-between">
                             <h2 class="text-2xl font-bold tracking-tight text-gray-900">${ element.title }</h2>
-                            <a href="${ element.url }" class="text-sm duration-150 text-gray-500 hover:text-gray-600">
+                            <a href="/category/${ element.url }" class="text-sm duration-150 text-gray-500 hover:text-gray-600">
                                 Смотреть все
                             </a>
                         </div>
@@ -24,6 +26,8 @@ function getPageData() {
                     `
                 )
             })
+
+            findWishlistBtns();
         },
         error: function(error) {
 
